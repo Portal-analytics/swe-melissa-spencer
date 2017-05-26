11ca5199427ea0a041afac14bc9922ac06a0db2f
Spencer Theocates Amarantides <sodadespencer@Austins-MBP.lan>
2017-05-26 10:57:06 -0400

    Errorous Version

--- day-2/startup-advice/src/App.js
+++ day-2/startup-advice/src/App.js
@@ -1,25 +1,32 @@
-import AppBar from 'material-url/AppBar';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 var React = require('react');
-
-const AppBarExampleIcon = () => (
-	<AppBar
+var AppBar = require('material-ui/AppBar');
+var MuiThemeProvider = require('material-ui/styles/MuiThemeProvider');
+/*
+function AppBarExampleIcon() {
+	return (
+		<AppBar
 		title="Great Fucking Startup Advice"
 		iconClassNameRight="muidocs-icon-navigation-expand-more"
 	/>
-);
+	)
+}
+*/
+
+// const AppBarExampleIcon = () => (
+// );
+
 
 class App extends React.Component {
-	  state = {
-			clicks: 0,
-			nextQuote:0
-    }
+	state = {
+		clicks: 0,
+		nextQuote: 0
+	}
 
 	nextQuote = () => {
-		this.setState({ 
+		this.setState({
 			clicks: this.state.clicks + 1,
-			nextQuote: this.state.nextQuote + 1 
+			nextQuote: this.state.nextQuote + 1
 		});
 	}
 
@@ -31,37 +38,37 @@ class App extends React.Component {
 	}
 
 	render() {
-    const advice = ['Fake it till you make it', 'Hire Spencer', 'Watch Silicon Valley', 'Hire Melissa', 'Read up on Bernie Madoff', 'Take a leap'];
+		const advice = ['Fake it till you make it', 'Hire Spencer', 'Watch Silicon Valley', 'Hire Melissa', 'Read up on Bernie Madoff', 'Take a leap'];
 		return (
-			<div className='container'>
-				<MuiThemeProvider>
-					<AppBarExampleIcon></AppBarExampleIcon>
-				</MuiThemeProvider>
-        <div>
-        	
-					<h1 className='quote'>
-						{advice[this.state.nextQuote % advice.length]}
-					</h1>
-					<div className='ButtonPanel'>
-						<button
-							className='button'
-							onClick={(e) => this.nextQuote(e)}>
-								In Order
+			<MuiThemeProvider>
+				<div>
+					<div className='container'>
+						<div>
+							<h1 className='quote'>
+								{advice[this.state.nextQuote % advice.length]}
+							</h1>
+							<div className='ButtonPanel'>
+								<button
+									className='button'
+									onClick={(e) => this.nextQuote(e)}>
+									In Order
 						</button>
-						<button
-							className='button'
-							onClick={(e) => this.randomQuote(advice, e)}>
-								Random
+								<button
+									className='button'
+									onClick={(e) => this.randomQuote(advice, e)}>
+									Random
 						</button>
+							</div>
+						</div>
+						<img className='image'
+							src="https://sites.psu.edu/behindthememes/files/2016/09/maxresdefault-1iulwsz.jpg"
+							alt="Just Do It" />
 					</div>
-      	</div>
-      	<img className='image'
-        	src="https://sites.psu.edu/behindthememes/files/2016/09/maxresdefault-1iulwsz.jpg"
-        	alt="Just Do It"/>
-				<h2 className='clicks'>
-					You have completed {this.state.clicks} clicks!
+					<h2 className='clicks'>
+						You have completed {this.state.clicks} clicks!
 				</h2>
-			</div>
+				</div>
+			</MuiThemeProvider>
 		)
 	}
 }
