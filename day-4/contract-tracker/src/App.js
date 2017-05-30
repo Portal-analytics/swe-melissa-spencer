6a4bec7d6e58554e746a6dc67385ae4b873af962
Spencer <sta7ey@virginia.edu>
2017-05-30 14:09:39 -0400

    Edit Feature

--- day-4/contract-tracker/src/App.js
+++ day-4/contract-tracker/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import PropTypes from 'prop-types';
 import './App.css';
 
-class PlayerInput extends Component {
+class UserInput extends Component {
   constructor (props) {
     super(props);
 
@@ -11,65 +11,42 @@ class PlayerInput extends Component {
       title: "",
       description: "",
       price: "",
-      contract: {}
     };
 
-    this.handleTitle = this.handleTitle.bind(this);
-    this.handleDescription = this.handleDescription.bind(this);
-    this.handlePrice = this.handlePrice.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateContractInput = this.updateContractInput.bind(this);
+    this.submitContract = this.submitContract.bind(this);
   }
 
-  handleSubmit(event) {
+  submitContract(event) {
     event.preventDefault();
+		this.props.onSubmit(this.state.title, this.state.description, this.state.price, this.props.editIndex);
 
-    this.state.contract = {
-      title: this.state.title, 
-      description: this.state.description, 
-      price: this.state.price,
-    };
-
-		this.props.onSubmit(this.state.contract);   //error
+    this.setState({
+      title: "",
+      description: "",
+      price: ""
+    });
   }
 
-  handleTitle(event) {
+  updateContractInput(field, event) {
 		var value = event.target.value;
 
 		this.setState(function() {
 			return {
         ...this.state,
-				title: value,
-			}
-		});
-	}
-
-  handleDescription(event) {
-		var value = event.target.value;
-
-		this.setState(function() {
-			return {
-        ...this.state,
-				description: value
-			}
-		});
-	}
-
-  handlePrice(event) {
-		var value = event.target.value;
-
-		this.setState(function() {
-			return {
-        ...this.state,
-        price: value
+				[field]: value,
 			}
 		});
 	}
 
   render() {
+    //debugger;
 		return (
-			<form className='column' onSubmit={this.handleSubmit}>
+			<form className='column' onSubmit={this.submitContract}>
+        {this.props.editIndex < 0 &&
+        <div>
 				<label className='header' htmlFor='title'>
-					New Contact
+					New Contract
 				</label>
 				<input
 					id='title'
@@ -77,7 +54,7 @@ class PlayerInput extends Component {
 					type='text'
 					autoComplete='off'
 					value={this.state.title}
-					onChange={this.handleTitle}
+					onChange={e => this.updateContractInput("title", e)}
 				/>
         <input
           id='description'
@@ -85,7 +62,7 @@ class PlayerInput extends Component {
 					type='text'
 					autoComplete='off'
 					value={this.state.description}
-					onChange={this.handleDescription}
+					onChange={e => this.updateContractInput("description", e)}
         />
         <input
           id='price'
@@ -93,14 +70,52 @@ class PlayerInput extends Component {
 					type='text'
 					autoComplete='off'
 					value={this.state.price}
-					onChange={this.handlePrice}
+					onChange={e => this.updateContractInput("price", e)}
         />
 				<button
 				className='button'
 				type='submit'
-				disabled={!this.state.title}>
+				disabled={!this.state.title && !this.state.description && !this.state.price}>
 					Submit
 				</button>
+        </div>}
+        
+        {this.props.editIndex >= 0 &&
+        <div>
+				<label className='header' htmlFor='title'>
+					Edit Contract {this.props.editIndex + 1}
+				</label>
+				<input
+					id='title'
+					placeholder={this.props.uploadedContract.title}
+					type='text'
+					autoComplete='off'
+					value={this.state.title}
+					onChange={e => this.updateContractInput("title", e)}
+				/>
+        <input
+          id='description'
+					placeholder={this.props.uploadedContract.description}
+					type='text'
+					autoComplete='off'
+					value={this.state.description}
+					onChange={e => this.updateContractInput("description", e)}
+        />
+        <input
+          id='price'
+					placeholder={this.props.uploadedContract.price}
+					type='text'
+					autoComplete='off'
+					value={this.state.price}
+					onChange={e => this.updateContractInput("price", e)}
+        />
+				<button
+				className='button'
+				type='submit'
+				disabled={!this.state.title && !this.state.description && !this.state.price}>
+					Finish Editing
+				</button>
+        </div>}
 			</form>
 		)
 	}
@@ -113,31 +128,101 @@ class App extends Component {
     super(props);
 
     this.state = {
-      contractList: []
+      contractList: [],
+      editIndex: -1,
+      editContract: {title: "", description: "", price: ""}
     };
+
+    this.createContract = this.createContract.bind(this);
+    this.uploadContract = this.uploadContract.bind(this);
+    this.editContract = this.editContract.bind(this);
   }
 
-  onSubmit (contact) {
-    this.state.contractList[this.state.contractList.length] = contact;
+
+  uploadContract (editIndex) {
+    this.setState({
+      ...this.state,
+      editIndex: editIndex,
+      editContract: this.state.contractList[editIndex]
+    });
+  }
+
+  editContract (title, description, price, index) {
+    let newContract = {
+      title: title,
+      description: description, 
+      price: price
+    };
+
+    console.log(newContract);
+    console.log(index);
+
+    let newContractList = this.state.contractList;
+    newContractList[index] = newContract;  //error
+
+    console.log(newContractList);
+
+    this.setState ({
+      ...this.state, 
+      contractList: newContractList,
+      editIndex: -1,
+      editContract: {title: "", description: "", price: ""}
+    });
+  }
+
+  createContract (title, description, price, index) {
+    let newContract = [{
+      title: title,
+      description: description, 
+      price: price
+    }];
+
+    let newContractList = this.state.contractList.concat(newContract);
+
+    this.setState ({
+      ...this.state, 
+      contractList: newContractList 
+    });
   }
 
   render() {
     return (
       <div className="App">
         <h1 className="Header">Contract Tracker</h1>
-        <PlayerInput id="input"/>
-        <ol className="contractList">
-          {this.state.contractList.map(contract =>
-            <li id={this.state.contractList.description} className="contact" >
+
+        {this.state.editIndex < 0 && 
+        <UserInput id="input" 
+          onSubmit={this.createContract}
+          editIndex={this.state.editIndex}
+          uploadedContract={this.state.editContract}
+          />}
+
+          {this.state.editIndex >= 0 && 
+        <UserInput id="input" 
+          onSubmit={this.editContract}
+          editIndex={this.state.editIndex}
+          uploadedContract={this.state.editContract}
+          />}
+
+        <ul className="contractList">
+          {this.state.contractList.map((contract, index) =>
+            <li key={index} className="contract" >
               <h1 className="title">
-                {this.state.contractList.title}
+                {contract.title}
               </h1>
-              <h3 className="price">
-                {this.state.contractList.price}
+              <h3 className="description">
+                Description: {contract.description}
               </h3>
+              <h3 className="price">
+                Price: {contract.price}
+              </h3>
+              <button className='button'
+									onClick={(e) => this.uploadContract(index)}>
+                Edit
+              </button>
             </li>
           )}
-        </ol>
+        </ul>
       </div>
     );
   }
